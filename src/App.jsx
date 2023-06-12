ab68bca7af3d0e42644f9fb4c37bf7447f199904
Phuong Anh <hkhag17@gmail.com>
2023-06-12 11:19:45 +0700

    mocking testing

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,29 @@ import Container from "./components/Container";
 import Row from "./components/Row";
 import Column from "./components/Column";
 import Form from "./components/Form";
+import Post from "./components/Post";
 
 const App = () => (
   <div>
     <Jumbotron>
       <TitleHeader className="text-center" title="React Form" />
     </Jumbotron>
-    <Container>
-      <Row>
-        <Column className="col-md-6 offset-md-3">
-          <Form />
-        </Column>
-      </Row>
-    </Container>
+    <div className="row">
+      <div className="col-6">
+        <Container>
+          <Row>
+            <Column className="col-md-6 offset-md-3">
+              <Form />
+            </Column>
+          </Row>
+        </Container>
+      </div>
+      <div className="col-6">
+        <Container>
+          <Post />
+        </Container>
+      </div>
+    </div>
   </div>
 );
 
