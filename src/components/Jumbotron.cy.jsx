e3204e13b3454a7d435008d6a3bdab3e8a02dc3a
Van Hien <hien.nv@kyanon.digital>
2023-06-20 09:23:53 +0700

    [feature] add allure report

--- src/components/Jumbotron.cy.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Jumbotron from './Jumbotron'
-
-describe('<Jumbotron />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<Jumbotron />)
-  })
-})
